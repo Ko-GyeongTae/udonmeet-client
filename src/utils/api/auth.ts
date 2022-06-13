a6063b71ae7f2fafae3ddd4950fa4c948e6f7e88
Ko-GyeongTae <kokt0203@naver.com>
2022-06-14 04:48:32 +0900

    Feat: api connect

---
+++ src/utils/api/auth.ts
@@ -0,0 +1,53 @@
+import { AxiosResponse } from "axios";
+import RequestApi from "../libs/requestApi";
+
+export interface SignInDto {
+  email: string;
+  password: string;
+}
+
+export interface SignUpDto {
+  email: string;
+  name: string;
+  password: string;
+}
+
+class AuthController {
+  signIn(payload: SignInDto): Promise<void | AxiosResponse> {
+    try {
+      return RequestApi({
+        url: "/user/sign-in",
+        method: "POST",
+        data: payload,
+      });
+    } catch (e: any) {
+      throw new Error(e);
+    }
+  }
+
+  signUp(payload: SignUpDto): Promise<void | AxiosResponse> {
+    try {
+      return RequestApi({
+        url: "/user/sign-up",
+        method: "POST",
+        data: payload,
+      });
+    } catch (e: any) {
+      throw new Error(e);
+    }
+  }
+
+  signOut(): Promise<void | AxiosResponse> {
+    try {
+      return RequestApi({
+        url: "/user/sign-out",
+        method: "POST",
+        isNeedToken: true,
+      });
+    } catch (e: any) {
+      throw new Error(e);
+    }
+  }
+}
+
+export default AuthController;
