b7e677186c11507799e376a26e83d77a5b33fe5c
Ko-GyeongTae <kokt0203@naver.com>
2022-06-14 04:50:47 +0900

    Feat: 인증여부에 따른 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./components/AuthProvider";
+import useAuth from "./hooks/useAuth";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  );
+function AuthenticatedRoute({ children }: { children: JSX.Element }) {
+  const { user } = useAuth();
+
+  if (!user) return <Navigate to="/login" />;
+
+  return children;
 }
 
-export default App;
+export default function App() {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/home"
+          element={
+            <AuthenticatedRoute>
+              <Home />
+            </AuthenticatedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
+  );
+}
